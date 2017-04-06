3d2042ea088825eb7c596a232c261e6b73abbf39
Sonia R <Sonia R>
2017-04-06 23:29:54 +0200

    console

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
 var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
 var animal = 'Zielone słonie';
-var animalUpperCased = animal.toUpperCase();
+var animalUpperCase = animal.toUpperCase();
 var textReplaced = text.replace('Papugi', animal);
-console.log(textReplaced);
-
 var partOfText = text.slice(0,36);
-console.log(partOfText);
+console.log(textReplaced + animalUpperCase + partOfText);
