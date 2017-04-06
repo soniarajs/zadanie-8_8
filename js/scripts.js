cc97bebabead6dabc68886b6633d7437186979fb
Sonia R <Sonia R>
2017-04-06 23:18:33 +0200

    newwork

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+var animal = 'Zielone słonie';
+var animalUpperCased = animal.toUpperCase();
+var textReplaced = text.replace('Papugi', animal);
+console.log(textReplaced);
+
+var partOfText = text.slice(0,36);
+console.log(partOfText);
