58beef0ef3934637d9ff62a72c99e60948ea1467
Sonia R <Sonia R>
2017-04-18 23:06:29 +0200

    popraiwone

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
 var animal = 'Zielone słonie';
 var animalUpperCase = animal.toUpperCase();
-var textReplaced = text.replace('Papugi', animal);
-var partOfText = text.slice(0,36);
-console.log(textReplaced + animalUpperCase + partOfText);
+var textReplaced = text.replace('Papugi', animalUpperCase);
+var partOfText = textReplaced.slice(0, textReplaced.length / 2);
+console.log(partOfText);
